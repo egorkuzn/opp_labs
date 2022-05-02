10d7b61e831eb5bfe743086b3155ebaba2b88ee7
egorkuzn <egorkuzn2002@mail.ru>
2022-05-03 01:22:29 +0700

    starting 3rd lab

---
+++ lab3/consistent_matrix_mul.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void MatrixRead(double* matrix, int lines, int columns, FILE* in){
+    //reading matrix 
+}
+
+void MatrixMul(double* A, double* B, double* C, int N, int M, int K){
+    for(int i = 0; i < N; ++i)
+        for(int j = 0; j < M; ++j)
+            for(int k = 0; k < K; ++k)
+                C[i * K + k] += A[i * M + j] * B[j * N + k];
+}
+
+void MatrixPrint(double* matrix, int lines, int columns){
+    for(int i = 0; i < lines; ++i){
+        for(int j = 0; j < columns; ++j)
+            printf("%lf ", matrix[i * columns + j]);
+        
+        printf("\n");
+    }
+}
+
+void MatrixFree(double* A, double* B, double* C){
+    free(A);
+    free(B);
+    free(C);
+}
+
+//     <----M---->                            <-K->
+//  ^  +----------+         <-K->             +---+ ^
+//  |  |          |         +---+ ^           |   | |
+//  |  |          |         |   | |    ____   |   | |
+//  N  |    A     |    X    | B | | M  ____   | C | | N
+//  |  |          |         |   | |           |   | |
+//  |  |          |         +---+ v           |   | |
+//  v  +----------+                           +---+ v
+
+int main(){
+    int N, M, K;
+// Input file praparation:
+    FILE* in = fopen("matrix.txt", "r");
+// Taking params:
+    fscanf(in, "%d %d %d", &N, &M, &K);
+// Memory allocation block:
+    double* A = (double*)calloc(N * M, sizeof(double));
+    double* B = (double*)calloc(M * K, sizeof(double));
+    double* C = (double*)calloc(N * K, sizeof(double));
+// Taking generated matrixes:
+    MatrixRead(A, N, M, in);
+    MatrixRead(B, M, K, in);
+// Finishing reading from file:
+    fclose(in);
+// Calculating C = A x B:
+    MatrixMul(A, B, C, N, M, K);
+// Results demonstration:
+    MatrixPrint(C, N, K);
+// Free block and finishing:    
+    MatrixFree(A, B, C);
+    return 0;
+}
---
+++ lab3/matrix_gen.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void GenMatrix(double* matrix, int lines, int columns){
+    srand(NULL);
+
+    for(int i = 0; i < lines; ++i)
+        for(int j = 0; j < columns; ++j)
+            matrix[i * columns + j] = rand() / (double)RAND_MAX * 50 - 25;
+}
+
+void WriteMatrix(double* matrix, int lines, int columns, FILE* out){
+    for(int i = 0; i < lines; ++i){
+        for(int j = 0; j < columns; ++j)
+            fprintf(out, "%lf ", matrix[i * columns + j]);
+        
+        fprintf(out, "\n");
+    }
+}
+
+//     <----M---->                            <-K->
+//  ^  +----------+         <-K->             +---+ ^
+//  |  |          |         +---+ ^           |   | |
+//  |  |          |         |   | |    ____   |   | |
+//  N  |    A     |    X    | B | | M  ____   | C | | N
+//  |  |          |         |   | |           |   | |
+//  |  |          |         +---+ v           |   | |
+//  v  +----------+                           +---+ v
+
+int main(int argc, char* argv[]){
+// Getting of matrixes sizes params: 
+    int N = atoi(argv[1]);
+    int M = atoi(argv[2]);
+    int K = atoi(argv[3]);
+// Memory allocation by zero:
+    double* A = (double*)calloc(N * M, sizeof(double));
+    double* B = (double*)calloc(M * K, sizeof(double));
+// Out file preparation:
+    FILE* out = fopen("matrix.txt", "w");
+    fprintf(out, "%d %d %d\n", N, M, K);
+// Generate matrix:
+    GenMatrix(A, N, M);
+    GenMatrix(B, M, K);
+// Writing matrix to out.txt file:
+    WriteMatrix(A, N, M, out);
+    WriteMatrix(B, M, K, out);
+// File closing, memory clearing and job finishing:
+    fclose(out);
+    free(A);
+    free(B);
+}
