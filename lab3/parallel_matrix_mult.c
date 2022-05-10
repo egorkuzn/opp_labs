0d70386162ca6cdae83a643b083889d25b4936e3
egorkuzn <egorkuzn2002@mail.ru>
2022-05-10 20:44:16 +0700

    finishing parallel program

--- lab3/parallel_matrix_mult.c
+++ lab3/parallel_matrix_mult.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 //Mask arrays:
-const bool HORIZONTAL[2] = {false, true},
-           VERTICAL  [2] = {true, false};
+const int HORIZONTAL[2] = {0, 1},
+           VERTICAL [2] = {1, 0};
 //Grid parameters box:
 struct grid{
     int height,
@@ -22,11 +23,11 @@ struct matrixes{
           * C;
 } matrixes;
 
-void Grid_Init(int height, int width){
-    grid.height = height;
-    grid.width = width;
-    grid.array[1] = height;
-    grid.array[2] = width;
+void Grid_Init(char* height, char* width){
+    grid.height = atoi(height);
+    grid.width  = atoi(width);
+    grid.array[1] = grid.height;
+    grid.array[2] = grid.width;
 }
 
 void MatrixRead(double* matrix, int lines, int columns, FILE* in){
@@ -42,22 +43,26 @@ void Matrixes_Init_NMK(FILE* in){
 
 void Matrixes_Init_AB(FILE* in, int* coords){
     if(coords[0] == 0 && coords[1] == 0){
-        MatrixRead(&matrixes.A, matrixes.N, matrixes.M, in);
-        MatrixRead(&matrixes.B, matrixes.N, matrixes.M, in);
+        MatrixRead(matrixes.A, matrixes.N, matrixes.M, in);
+        MatrixRead(matrixes.B, matrixes.N, matrixes.M, in);
     }
 }
 
-void Matrix_B_Scatter(MPI_Comm* COMM_2D, MPI_Datatype* B_TYPE, int* coords, int* root, double* B_part){
-    int coords_to_receive[2] = {0, 0};
+void Matrix_B_Scatter(MPI_Comm* COMM_2D, int* coords, int* root, double* B_part){
+    int rank_recieve,
+        coords_to_receive[2] = {0, 0};
+
+    MPI_Datatype B_TYPE;
+    MPI_Type_vector(matrixes.M, matrixes.K / grid.width, matrixes.K, MPI_DOUBLE, &B_TYPE);
+    MPI_Type_commit(&B_TYPE);
     MPI_Cart_rank(*COMM_2D, coords_to_receive, root);
 
     for(int i = 1; i < grid.width; ++i){
-        int rank_recieve;
         coords_to_receive[1] = i;
         MPI_Cart_rank(*COMM_2D, coords_to_receive, &rank_recieve);
 
         if(coords[0] == 0 && coords[1] == 0)
-            MPI_Send(&matrixes.B[i * matrixes.K / grid.width], 1, *B_TYPE, rank_recieve, i, *COMM_2D);
+            MPI_Send(&matrixes.B[i * matrixes.K / grid.width], 1, B_TYPE, rank_recieve, i, *COMM_2D);
     }
 
     if (coords[0] == 0 && coords[1] != 0)
@@ -65,7 +70,9 @@ void Matrix_B_Scatter(MPI_Comm* COMM_2D, MPI_Datatype* B_TYPE, int* coords, int*
     
     if (coords[0] == 0 && coords[1] == 0)
         for (int i = 0; i < matrixes.M; ++i) 
-            memcpy(&B_part[i * matrixes.K / grid.width], &matrixes.B[i * matrixes.K], matrixes.K / grid.width * sizeof(double));      
+            memcpy(&B_part[i * matrixes.K / grid.width], &matrixes.B[i * matrixes.K], matrixes.K / grid.width * sizeof(double));  
+
+    MPI_Type_free(B_TYPE);    
 }
 
 void Matrix_C_Gather(int root, int processRank, double* C_part, MPI_Comm* COMM_2D){
@@ -74,7 +81,7 @@ void Matrix_C_Gather(int root, int processRank, double* C_part, MPI_Comm* COMM_2
     MPI_Type_commit(&C_TYPE);
 
     if (processRank != root)
-        MPI_Send(C_part, matrixes.N / grid.height * matrixes.K / grid.width, MPI_DOUBLE, root, processRank, COMM_2D);
+        MPI_Send(C_part, matrixes.N / grid.height * matrixes.K / grid.width, MPI_DOUBLE, root, processRank, *COMM_2D);
 
     int coords_src[2] = {0, 0};
 
@@ -84,8 +91,8 @@ void Matrix_C_Gather(int root, int processRank, double* C_part, MPI_Comm* COMM_2
                 int rank_src;
                 coords_src[0] = i;
                 coords_src[1] = j;
-                MPI_Cart_rank(COMM_2D, coords_src, &rank_src);
-                MPI_Recv(&matrixes.C[i * matrixes.N / grid.height * matrixes.K + j * matrixes.K / grid.width], 1, C_TYPE, rank_src, rank_src, COMM_2D, MPI_STATUS_IGNORE);
+                MPI_Cart_rank(*COMM_2D, coords_src, &rank_src);
+                MPI_Recv(&matrixes.C[i * matrixes.N / grid.height * matrixes.K + j * matrixes.K / grid.width], 1, C_TYPE, rank_src, rank_src, *COMM_2D, MPI_STATUS_IGNORE);
             }
 
     if (processRank == root)
@@ -103,7 +110,7 @@ void MatrixMul(double* A, double* B, double* C, int N, int M, int K){
                 C[i * K + k] += A[i * M + j] * B[j * N + k];
 }
 
-void final_free(double* A_part, double* B_part, double* C_part, MPI_Datatype* B_TYPE, MPI_Comm* COMM_HORIZONTAL, MPI_Comm* COMM_VERTICAL, MPI_Comm* COMM_2D){
+void final_free(double* A_part, double* B_part, double* C_part, MPI_Comm* COMM_HORIZONTAL, MPI_Comm* COMM_VERTICAL, MPI_Comm* COMM_2D){
     free(matrixes.A);
     free(matrixes.B);
     free(matrixes.C);
@@ -112,18 +119,21 @@ void final_free(double* A_part, double* B_part, double* C_part, MPI_Datatype* B_
     free(B_part);
     free(C_part);
 
-    MPI_Type_free(B_TYPE);
-
     MPI_Comm_free(COMM_HORIZONTAL);
     MPI_Comm_free(COMM_VERTICAL);
     MPI_Comm_free(COMM_2D);
 }
 
 void final_printf(double start, double end){
-
+    printf("N:%d\tcolumns%lf\n\
+            M:%d\tlines%lf\n\
+            K:%d\ttime:%lf\n", matrixes.N, grid.width,
+                               matrixes.M, grid.height,
+                               matrixes.K, start - end);
 }
-//    A                 B             C
 
+//    A                 B             C
+//
 // <---M--->          <-K->         <-K->
 //  ______             ___           ___
 // |______| ^         | |X| ^       | | | ^
@@ -151,7 +161,7 @@ int main(int argc, char* argv[]){
     int periods[2] = {0, 0},
         coords [2];
     MPI_Comm COMM_2D, COMM_HORIZONTAL, COMM_VERTICAL;
-    MPI_Cart_create(MPI_COMM_WORLD, 2, grid.array, periods, true, &COMM_2D);
+    MPI_Cart_create(MPI_COMM_WORLD, 2, grid.array, periods, false, &COMM_2D);
     MPI_Cart_coords(COMM_2D, processRank, 2, coords);
     MPI_Cart_sub(COMM_2D, HORIZONTAL, &COMM_HORIZONTAL);
     MPI_Cart_sub(COMM_2D, VERTICAL, &COMM_VERTICAL);
@@ -172,13 +182,10 @@ int main(int argc, char* argv[]){
     MPI_Scatter(matrixes.A, matrixes.M * matrixes.N / grid.height, MPI_DOUBLE,
                     A_part, matrixes.M * matrixes.N / grid.height, MPI_DOUBLE, 0, COMM_VERTICAL);
     MPI_Bcast(A_part, matrixes.M * matrixes.N / grid.height, MPI_DOUBLE, 0, COMM_HORIZONTAL);
-//Taking part of matrix B: vvvvvvvvvvvvvvvvvvvvvvvvТУТ ЕЩЁ ЕСТЬ ВОЗМОЖНОСТЬ НАВЕСТИ КРАСОТУvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+//Taking part of matrix B:
     double* B_part = (double*)calloc(matrixes.K / grid.width  * matrixes.M, sizeof(double));
-    MPI_Datatype B_TYPE;
-    MPI_Type_vector(matrixes.M, matrixes.K / grid.width, matrixes.K, MPI_DOUBLE, &B_TYPE);
-    MPI_Type_commit(&B_TYPE);
     int root;
-    Matrix_B_Scatter(&COMM_2D, &B_TYPE, coords, &root, B_part);
+    Matrix_B_Scatter(&COMM_2D, coords, &root, B_part);
     MPI_Bcast(B_part, matrixes.M * matrixes.K / grid.width, MPI_DOUBLE, 0, COMM_VERTICAL);
 //Taking part of matrix C:
     double* C_part = (double*)calloc(matrixes.N / grid.height * matrixes.K / grid.width, sizeof(double));
@@ -189,7 +196,7 @@ int main(int argc, char* argv[]){
 //                     END
     double end = MPI_Wtime();
 //Memory deallocation:
-    final_free(A_part, B_part, C_part, &B_TYPE, &COMM_HORIZONTAL, &COMM_VERTICAL, &COMM_2D);
+    final_free(A_part, B_part, C_part, &COMM_HORIZONTAL, &COMM_VERTICAL, &COMM_2D);
 //Final output:
     if(processRank == 0)
         final_printf(start, end);
